2c9bc08bdb72c9f6c56ef8b8269b605e93d92781
ABDO-AR <arcompany000@gmail.com>
2021-04-09 02:44:21 +0200

    Set The System States, Navigation Bar Color in the main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:ar_todo_app/dev/home_screen_dev/home_dev.dart';
 import 'package:ar_todo_app/res/screens/home_screen_res/home_res.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -19,6 +20,17 @@ class ARTODOAPP extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    /// Set The System States, Navigation Bar Color
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle.dark.copyWith(
+        statusBarColor: Color(0xFF212121),
+        systemNavigationBarDividerColor: Color(0xFF212121),
+        systemNavigationBarColor: Color(0xFF212121),
+        systemNavigationBarIconBrightness: Brightness.dark,
+        statusBarIconBrightness: Brightness.dark,
+        statusBarBrightness: Brightness.dark,
+      ),
+    );
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'ARTODOAPP',
