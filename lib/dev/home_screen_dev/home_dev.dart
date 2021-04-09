c731ab018a2a204d7a1a0d7b40a29fce8a19552a
ABDO-AR <arcompany000@gmail.com>
2021-04-09 02:35:44 +0200

    add getter Counter

--- lib/dev/home_screen_dev/home_dev.dart
+++ lib/dev/home_screen_dev/home_dev.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class HomeScreenDev extends ChangeNotifier {
   int testCounter = 0;
+  int get getCounter => testCounter;
 
   void addCounter() {
     testCounter++;
